1fcb919f80d87e7d4dc4d32a0e9131c6fe947772
moor-ankh-magick <phoniquevago@gmail.com>
2025-07-18 05:57:01 +0200

    Add .next/server/app/api/trading/market/route.js 1752811020960

---
+++ .next/server/app/api/trading/market/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/trading/market/route.js";
+const runtime = require("../../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_1b6a3847._.js");
+runtime.loadChunk("server/chunks/[root-of-the-server]__4ad68d07._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/trading/market/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
+runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/trading/market/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/trading/market/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
